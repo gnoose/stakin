784e617deaf5e383a59ca4a8f9f644416fb31bc4
gnoose <jieli3838@gmail.com>
2021-11-18 03:44:34 +0900

    feat: tailwindcss config completed  - dialog context added

---
+++ components/ui-kit/dialog/dialog.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, Children, cloneElement, isValidElement } from 'react';
+import Modal from 'react-modal';
+import { modalCustomStyles } from './types';
+
+Modal.setAppElement('#__next');
+
+interface Props {
+  isOpen: boolean;
+  closeDialog: () => void;
+  children: ReactNode;
+}
+
+export function Dialog(props: Props) {
+  return (<Modal
+    isOpen={props.isOpen}
+    style={modalCustomStyles}
+    shouldCloseOnOverlayClick={true}
+    shouldCloseOnEsc={true}
+    onRequestClose={props.closeDialog}
+    closeTimeoutMS={300}
+  >
+    {Children.map(props.children, child => {
+      if (isValidElement(child)) {
+        return cloneElement(child, { closeDialog: props.closeDialog });
+      }
+      return child;
+    })}
+  </Modal>);
+}
